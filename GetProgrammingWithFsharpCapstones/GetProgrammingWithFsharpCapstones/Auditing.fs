633ebe76b564d7376bcf0cd353fa2674618db66c
ToKra <kratas.tom@gmail.com>
2020-04-21 21:40:54 +0200

    add auditing.fs

---
+++ GetProgrammingWithFsharpCapstones/GetProgrammingWithFsharpCapstones/Auditing.fs
@@ -0,0 +1,16 @@
+﻿module Auditing
+open Domain
+open System
+open System.IO
+
+let fileSystemAudit account message =
+    let fileName =  account.AccountId.ToString() + ".txt"
+    let folderpath = sprintf "C:\\temp\\learnfs\\capstone2\\%s" account.Owner.Name
+    let filePath = Path.Combine(folderpath, fileName)
+    let createDirectory = Directory.CreateDirectory(folderpath)
+    let writeToFile = File.AppendAllText(filePath, message + "\n") // lol first line is empty
+    ()
+
+let consoleAudit account message =
+    Console.WriteLine("Account " + account.AccountId.ToString() + ": " + message)
+    ()
