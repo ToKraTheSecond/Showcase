0d18e4e43706735b866d4438043e2f2627342a60
ToKra <kratas.tom@gmail.com>
2020-04-21 21:14:31 +0200

    add operations.fs

---
+++ GetProgrammingWithFsharpCapstones/GetProgrammingWithFsharpCapstones/Operations.fs
@@ -0,0 +1,24 @@
+﻿module Operations
+open Domain
+
+let deposit amount account =
+    {
+        AccountId = account.AccountId;
+        Owner = account.Owner;
+        Balance = account.Balance + amount
+    }
+
+let withdraw amount account =
+    if amount > account.Balance then
+        account
+    else
+        {
+            AccountId = account.AccountId;
+            Owner = account.Owner;
+            Balance = account.Balance - amount
+        }
+
+let auditAs operationName audit operation amount account =
+    let account = operation amount account
+    audit account ("activity: " + operationName + " " + amount.ToString() + "; current amount: " + account.Balance.ToString())    
+    account
