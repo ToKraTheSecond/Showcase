f8c0f475bbbe13a68b4b0544b744663708760972
ToKra <kratas.tom@gmail.com>
2020-04-22 23:02:47 +0200

    move program.fs code from gitlab

--- GetProgrammingWithFsharpCapstones/GetProgrammingWithFsharpCapstones/Program.fs
+++ GetProgrammingWithFsharpCapstones/GetProgrammingWithFsharpCapstones/Program.fs
@@ -1,6 +1,42 @@
-﻿
+﻿open System
+open Domain
+open Operations
+open Auditing
+
+// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+let getCustomerName() =
+    Console.Write("Enter name: ")
+    Console.ReadLine()
+
+let getAmount() =
+    Console.Write("Enter amount: ")
+    Decimal.Parse(Console.ReadLine())
+
+let getOperation() =
+    Console.Write("Enter operation: ")
+    Console.ReadLine()
+
+let withdrawWithConsoleAudit = auditAs "withdraw" consoleAudit withdraw
+let depositWithConsoleAudit = auditAs "deposit" consoleAudit deposit
 
 [<EntryPoint>]
 let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+    let name = getCustomerName()
+    let balance = getAmount()
+
+    let cus = { Name = name }
+    let mutable acc = { AccountId = Guid.NewGuid(); Owner = cus; Balance = balance }
+
+    while true do
+        let operation = getOperation()
+        if operation = "exit" then Environment.Exit 0
+        let balance = getAmount()
+        acc <-
+            match operation with
+            | "deposit" ->  depositWithConsoleAudit balance acc
+            | "withdraw" -> withdrawWithConsoleAudit balance acc
+            | _ -> acc
+        ()
+    0
