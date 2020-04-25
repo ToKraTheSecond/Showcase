a4eedc3a4239f5b3462cc8aab87fb49f94a986d3
ToKra <kratas.tom@gmail.com>
2020-04-25 15:27:35 +0200

    move helper script from gitlab to github

---
+++ GetProgrammingWithFsharpCapstones/GetProgrammingWithFsharpCapstones/helper.fsx
@@ -0,0 +1,32 @@
+﻿#load "Domain.fs"
+#load "Operations.fs"
+#load "Auditing.fs"
+
+open System
+open Domain
+open Operations
+open Auditing
+
+// Dummy test cus1 and acc1
+
+let cus1 = { Name = "Isaac" }
+let acc1 = { AccountId = Guid.NewGuid(); Owner = cus1; Balance = 100M }
+
+// Test out withdraw
+
+let newAccount = acc1 |> withdraw 10M
+newAccount.Balance = 80M // should be true
+
+// Test out console auditor
+
+consoleAudit acc1 "Testing console audit"
+
+// Composite magic
+acc1 |> deposit 100M |> withdraw 50M
+
+let withdrawWithConsoleAudit = auditAs "withdraw" consoleAudit withdraw
+let depositWithConsoleAudit = auditAs "deposit" consoleAudit deposit
+
+acc1
+|> depositWithConsoleAudit 100M
+|> withdrawWithConsoleAudit 50M
