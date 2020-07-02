04810b782e40a9ee31e4b5e83cdcbf6c6599afee
ToKra <kratas.tom@gmail.com>
2020-07-02 22:58:17 +0200

    add Operations.fs with code from previous capstone

---
+++ GetProgrammingWithFsharpCapstones/Capstone3/Operations.fs
@@ -0,0 +1,30 @@
+﻿module Operations
+
+open System
+open Domain
+
+let deposit amount account =
+    { account with Balance = account.Balance + amount }
+
+let withdraw amount account =
+    if amount > account.Balance then 
+        Console.WriteLine("Transaction rejected!")
+        account
+    else { account with Balance = account.Balance - amount }
+
+let auditAs operationName audit operation amount account =
+    let updatedAccount = operation amount account
+    audit account (" balance: " + updatedAccount.Balance.ToString())
+    updatedAccount
+
+let getCustomerName() =
+    Console.WriteLine("Enter name: ")
+    Console.ReadLine()
+
+let getAmount() =
+    Console.WriteLine("Enter amount (positive double with dot delimiter): ")
+    Decimal.Parse(Console.ReadLine())
+
+let getOperation() =
+    Console.WriteLine("Choose operation [deposit | withdraw | exit]: ")
+    Console.ReadLine()
