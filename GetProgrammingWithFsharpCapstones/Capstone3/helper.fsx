b714228aadfe011fdb2b4dd5f38016049e6711f7
ToKra <kratas.tom@gmail.com>
2020-07-04 21:37:40 +0200

    clean helper; add dummyTransactions to helper

--- GetProgrammingWithFsharpCapstones/Capstone3/helper.fsx
+++ GetProgrammingWithFsharpCapstones/Capstone3/helper.fsx
@@ -5,31 +5,9 @@ open System
 open Domain
 open Operations
 
-let isValidCommand command =
-    let validCommands = Set.ofList [ 'd'; 'w'; 'e']
-    validCommands.Contains(command)
-
-let isStopCommand command =
-    command.ToString().Equals('e')
-
-let getAmount command =
-    match command with
-    | 'd' -> ('d', 50M)
-    | 'w' -> ('w', 25M)
-    | 'e' -> ('e', 0M)
-
-let processCommand (account:Account) (command:char, amount:decimal) =
-    match command with
-    | 'd' -> deposit amount account
-    | 'w' -> withdraw amount account
-    | 'e' -> account
-
-let openingAccount = { AccountId = Guid.Empty; Owner = { Name = "Isaac"}; Balance = 0M }
-let account =
-    let commands = [ 'd'; 'w'; 'z'; 'f'; 'd'; 'x'; 'w']
-
-    commands
-    |> Seq.filter isValidCommand
-    |> Seq.takeWhile (not << isStopCommand)
-    |> Seq.map getAmount
-    |> Seq.fold processCommand openingAccount
+let dummyTransactions =
+    [
+        {Amount = 25M; Operation = "deposit"; Timestamp = "moc"; WasSuccess = true};
+        {Amount = 5M; Operation = "withdraw"; Timestamp = "moc"; WasSuccess = true};
+        {Amount = 25M; Operation = "withdraw"; Timestamp = "moc"; WasSuccess = true};
+    ]
