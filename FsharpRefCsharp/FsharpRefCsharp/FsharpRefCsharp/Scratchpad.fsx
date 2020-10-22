026bca71d343c190e087038229b9730c89d33a95
ToKra <kratas.tom@gmail.com>
2020-10-22 22:06:20 +0200

    add FsharpRefCsharp project

---
+++ FsharpRefCsharp/FsharpRefCsharp/FsharpRefCsharp/Scratchpad.fsx
@@ -0,0 +1,16 @@
+﻿// requires absolute path
+// not sure how to use relative path
+#r @"C:\Users\krata\git\FsharpShowcase\FsharpRefCsharp\FsharpRefCsharp\CSharpProject\obj\Debug\netstandard2.0\CSharpProject.dll"
+
+open CSharpProject
+
+let simon = Person "Simon"
+simon.PrintName()
+
+let longhand =
+    [ "Tony"; "Fred"; "Samantha"; "Brad"; "Sophie"]
+    |> List.map(fun name -> Person(name)) // calling a constructor explicitly
+
+let shorthand =
+    [ "Tony"; "Fred"; "Samantha"; "Brad"; "Sophie"]
+    |> List.map Person // treating a constructor like a standard function
