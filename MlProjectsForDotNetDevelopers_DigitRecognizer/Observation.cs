337c8f1fcea57b81d182a09fd34132ebd205b8c6
ToKra <kratas.tom@gmail.com>
2020-10-25 20:49:26 +0100

    add observation class

---
+++ MlProjectsForDotNetDevelopers_DigitRecognizer/Observation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_DigitRecognizer
+{
+    class Observation
+    {
+        public Observation(string label, int[] pixels)
+        {
+            this.Label = label;
+            this.Pixels = pixels;
+        }
+
+        public string Label { get; private set; }
+        public int[] Pixels { get; private set; }
+    }
+}
