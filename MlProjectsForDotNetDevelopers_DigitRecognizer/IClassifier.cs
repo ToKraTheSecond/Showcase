8ef06f69f15e36391717011013ea5a00924eff83
ToKra <kratas.tom@gmail.com>
2020-10-25 22:09:43 +0100

    add classifier interface

---
+++ MlProjectsForDotNetDevelopers_DigitRecognizer/IClassifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp_DigitRecognizer
+{
+    public interface IClassifier
+    {
+        internal void Train(IEnumerable<Observation> trainingSet);
+        string Predict(int[] pixels);
+    }
+}
