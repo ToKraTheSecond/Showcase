11d596fd0072240f6f1654511a4eeedff981fc60
ToKra <kratas.tom@gmail.com>
2020-10-27 15:21:42 +0100

    add data reader to script

--- FSharp/script.fsx
+++ FSharp/script.fsx
@@ -1,1 +1,16 @@
-﻿
+﻿open System.IO
+
+type Observation = { Label:string; Pixels:int[] }
+
+let toObservation (csvData:string) =
+    let columns = csvData.Split(',')
+    let label = columns.[0]
+    let pixels = columns.[1..] |> Array.map int
+    { Label = label; Pixels = pixels }
+
+let reader path =
+    let data = File.ReadAllLines path
+    data.[1..] |> Array.map toObservation
+
+let trainingPath = @"C:\Users\krata\git\FsharpShowcase\MlProjectsForDotNetDevelopers_DigitRecognizer\data\trainingsample.csv" // update this path
+let trainingData = reader trainingPath
