480751dc66e48c1d58a879d72872dcee7b19686d
ToKra <kratas.tom@gmail.com>
2020-10-27 15:00:03 +0100

    use implemented classes in main

--- MlProjectsForDotNetDevelopers_DigitRecognizer/Program.cs
+++ MlProjectsForDotNetDevelopers_DigitRecognizer/Program.cs
@@ -7,8 +7,18 @@ namespace DigitRecognizer
     {
         static void Main(string[] args)
         {
+            var distance = new ManhattanDistance();
+            var classifier = new BasicClassifier(distance);
+
             var trainingPath = "data/trainingsample.csv";
             var training = DataReader.ReadObservations(trainingPath);
+            classifier.Train(training);
+
+            var validationPath = "data/validationsample.csv";
+            var validation = DataReader.ReadObservations(validationPath);
+
+            var correct = Evaluator.Correct(validation, classifier);
+            Console.WriteLine("Correctly classified: {0:P2}", correct);
 
             Console.ReadLine();
         }
