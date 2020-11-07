30697c00eba90e45c9735cba373a43919fdddd9b
ToKra <kratas.tom@gmail.com>
2020-11-07 15:03:48 +0100

    load dataset in script

--- MlProjectsForDotNetDevelopers_SmsSpamDetection/SmsSpamDetection/Script.fsx
+++ MlProjectsForDotNetDevelopers_SmsSpamDetection/SmsSpamDetection/Script.fsx
@@ -1,1 +1,23 @@
-﻿
+﻿open System.IO
+
+type DocType =
+    | Ham
+    | Spam
+
+let datasetPath = Path.Combine(__SOURCE_DIRECTORY__, "Data", "SMSSpamCollection")
+
+let parseDocType (label:string) =
+    match label with
+    | "ham" -> Ham
+    | "spam" -> Spam
+    | _ -> failwith "Unknown label"
+
+let parseLine (line:string) =
+    let split = line.Split('\t')
+    let label = split.[0] |> parseDocType
+    let message = split.[1]
+    (label, message)
+
+let dataset =
+    File.ReadAllLines datasetPath
+    |> Array.map parseLine
