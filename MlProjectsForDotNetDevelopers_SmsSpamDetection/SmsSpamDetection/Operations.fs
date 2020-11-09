1e2a13b5880256602431ee7957f5c3ad66cba524
ToKra <kratas.tom@gmail.com>
2020-11-09 18:50:47 +0100

    add analyze method

--- MlProjectsForDotNetDevelopers_SmsSpamDetection/SmsSpamDetection/Operations.fs
+++ MlProjectsForDotNetDevelopers_SmsSpamDetection/SmsSpamDetection/Operations.fs
@@ -3,8 +3,26 @@
 open Domain
 
 let proportion count total = float count / float total
+
 let laplace count total = float (count+1) / float (total+1)
+
 let countIn (group:TokenizedDoc seq) (token:Token) =
     group
     |> Seq.filter (Set.contains token)
     |> Seq.length
+
+let analyze (group:TokenizedDoc seq) (totalDocs:int) (classificationTokens:Token Set) =
+    let groupSize = group |> Seq.length
+    let score token =
+        let count = countIn group token
+        laplace count groupSize
+    let scoredTokens =
+        classificationTokens
+        |> Set.map (fun token -> token, score token)
+        |> Map.ofSeq
+    let groupProportion = proportion groupSize totalDocs
+
+    {
+        Proportion = groupProportion
+        TokenFrequencies = scoredTokens
+    }
