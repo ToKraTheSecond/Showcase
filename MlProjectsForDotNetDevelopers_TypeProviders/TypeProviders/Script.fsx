a234a05b1ba8c0952e9ab862d20180a9212a968a
ToKra <kratas.tom@gmail.com>
2020-11-21 21:05:43 +0100

    get SO questions from SO API

--- MlProjectsForDotNetDevelopers_TypeProviders/TypeProviders/Script.fsx
+++ MlProjectsForDotNetDevelopers_TypeProviders/TypeProviders/Script.fsx
@@ -1,1 +1,10 @@
-﻿
+﻿#I @"C:\Users\krata\.nuget\packages\" // TODO reference it from solution folder
+#r @"fsharp.data\3.3.3\lib\net45\FSharp.Data.dll"
+
+open FSharp.Data
+
+type Questions = JsonProvider<"""https://api.stackexchange.com/2.2/questions?site=stackoverflow""">
+
+let csQuestions = """https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=C%23"""
+
+Questions.Load(csQuestions).Items |> Seq.iter (fun q -> printfn "%s" q.Title)
