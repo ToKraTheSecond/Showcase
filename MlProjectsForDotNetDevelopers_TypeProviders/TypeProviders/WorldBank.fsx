dc3b66e7335a98b13a5e5773c7aeea60145772cc
ToKra <kratas.tom@gmail.com>
2020-11-27 21:57:55 +0100

    import needed dependencies

--- MlProjectsForDotNetDevelopers_TypeProviders/TypeProviders/WorldBank.fsx
+++ MlProjectsForDotNetDevelopers_TypeProviders/TypeProviders/WorldBank.fsx
@@ -1,4 +1,10 @@
 ﻿#I @"C:\Users\krata\.nuget\packages\" // TODO reference it from solution folder
 #r @"fsharp.data\3.3.3\lib\net45\FSharp.Data.dll"
+#r @"r.net\1.7.0\lib\net40\RDotNet.dll"
+#r @"rprovider\1.1.22\lib\net40\RProvider.Runtime.dll"
+#r @"rprovider\1.1.22\lib\net40\RProvider.dll"
 
-
+open FSharp.Data
+open RProvider
+open RProvider.``base``
+open RProvider.graphics
