5aeb3bf8697dee1c4654aca33ae1859bc62e863b
ToKra <kratas.tom@gmail.com>
2020-11-28 12:58:48 +0100

    create and plot an R data frame

--- MlProjectsForDotNetDevelopers_TypeProviders/TypeProviders/WorldBank.fsx
+++ MlProjectsForDotNetDevelopers_TypeProviders/TypeProviders/WorldBank.fsx
@@ -22,3 +22,21 @@ R.summary(surface) |> R.print
 R.hist(surface)
 R.hist(surface |> R.log)
 R.plot(surface, pop2010)
+
+// Creating and plotting an R data frame
+let pollution = [ for c in countries -> c.Indicators.``CO2 emissions (kt)``.[2000]]
+let education = [ for c in countries -> c.Indicators.``School enrollment, secondary (% gross)``.[2000]]
+
+let rdf =
+    [
+        "Pop2000", box pop2000
+        "Pop2010", box pop2010
+        "Surface", box surface
+        "Pollution", box pollution
+        "Education", box education ]
+    |> namedParams
+    |> R.data_frame
+
+// Scatterplot of all features
+rdf |> R.plot
+rdf |> R.summary |> R.print
