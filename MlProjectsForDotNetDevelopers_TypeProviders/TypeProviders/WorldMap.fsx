6f1914f16f679a7781f9ef7b7cace4a2ca57cfb3
ToKra <kratas.tom@gmail.com>
2020-11-28 15:32:50 +0100

    add new script and load needed dependencies

---
+++ MlProjectsForDotNetDevelopers_TypeProviders/TypeProviders/WorldMap.fsx
@@ -0,0 +1,6 @@
+﻿#I @"C:\Users\krata\.nuget\packages\" // TODO reference it from solution folder
+#r @"fsharp.data\3.3.3\lib\net45\FSharp.Data.dll"
+#r @"deedle\2.3.0\lib\net45\Deedle.dll"
+
+open FSharp.Data
+open Deedle
