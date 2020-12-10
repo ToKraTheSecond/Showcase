0e5735d63dad95a4e36523181d0757dc2a7a4339
ToKra <kratas.tom@gmail.com>
2020-12-10 19:44:34 +0100

    install FSharp.Charting; add script with lib import and basic plot

---
+++ MlProjectsForDotNetDevelopers_Regression/Script.fsx
@@ -0,0 +1,12 @@
+﻿#I @"C:\Users\krata\.nuget\packages\" // TODO reference it from solution folder
+#r @"fsharp.data\3.3.3\lib\net45\FSharp.Data.dll"
+#load @"fsharp.charting\2.1.0\FSharp.Charting.fsx"
+
+open FSharp.Data
+open FSharp.Charting
+
+type Data = CsvProvider<"Data/day.csv">
+let dataset = Data.Load("Data/day.csv")
+let data = dataset.Rows
+
+let all = Chart.Line [ for obs in data -> obs.Cnt ]
