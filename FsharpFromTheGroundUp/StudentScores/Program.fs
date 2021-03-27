d8033caedbddb7194bb83f58383080c5c647a038
ToKra <kratas.tom@gmail.com>
2021-03-27 12:52:33 +0100

    16. collection funcs recap

--- FsharpFromTheGroundUp/StudentScores/Program.fs
+++ FsharpFromTheGroundUp/StudentScores/Program.fs
@@ -1,13 +1,38 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
 open System
+open System.IO
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let printMeanScore (row : string) =
+    let elements = row.Split('\t')
+    let name = elements.[0]
+    let id = elements.[1]
+    let scores =
+        elements
+        |> Array.skip 2
+        |> Array.map float 
+    let meanScore = scores |> Array.average
+    let minScore = scores |> Array.min
+    let maxScore = scores |> Array.max
+    printfn "%s\t%s\t%0.1f\t%0.1f\t%0.1f" name id meanScore minScore maxScore
+
+let summerize filePath =
+    let rows = File.ReadAllLines filePath
+    let studentCount = (rows |> Array.length) - 1
+    printfn "Student count %i" studentCount
+    rows
+    |> Array.skip 1
+    |> Array.iter printMeanScore
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    if argv.Length = 1 then
+        let filePath = argv.[0]
+        if File.Exists filePath then
+            printfn "Processing %s" filePath
+            summerize filePath
+            0
+        else
+            printfn "File not found: %s" filePath
+            2
+    else
+        printfn "Please specify a file."
+        1
