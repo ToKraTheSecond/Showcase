fc561db70e56ef98dcef7c86d2f03ca472c23390
ToKra <kratas.tom@gmail.com>
2021-03-29 20:05:08 +0200

    handling exception - try / with

--- FsharpFromTheGroundUp/StudentScores/Program.fs
+++ FsharpFromTheGroundUp/StudentScores/Program.fs
@@ -10,11 +10,24 @@ let main argv =
         match (File.Exists filePath) with
         | true ->
             printfn "Processing %s" filePath
-            Summary.summerize filePath
-            0
+            try
+                Summary.summerize filePath
+                0
+            with
+            | :? FormatException as e ->
+                printfn "Error: %s" e.Message
+                printfn "The file was not in the expected format."
+                1
+            | :? IOException as e ->
+                printfn "Error: %s" e.Message
+                printfn "The file is open in another program, please close it."
+                2
+            | _ as e ->
+                printfn "Unexpected error: %s" e.Message
+                3
         | _ ->
             printfn "File not found: %s" filePath
-            2
+            4
     | _ ->
         printfn "Please specify a file."
-        1
+        5
