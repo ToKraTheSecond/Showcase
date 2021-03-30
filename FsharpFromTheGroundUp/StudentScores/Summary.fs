71050c305f9e98ef1236431104c39ac1f0f36eb2
ToKra <kratas.tom@gmail.com>
2021-03-30 20:39:45 +0200

    collection functions

--- FsharpFromTheGroundUp/StudentScores/Summary.fs
+++ FsharpFromTheGroundUp/StudentScores/Summary.fs
@@ -4,6 +4,15 @@ module Summary =
 
     open System.IO
 
+    let printGroupSummary (surname : string) (students : Student[]) =
+        printfn "%s" (surname.ToUpperInvariant())
+        students
+        |> Array.sortBy (fun student ->
+            student.GivenName, student.Id)
+        |> Array.iter (fun student ->
+            printfn "\t%20s\t%s\t%0.1f\t%0.1f\t%0.1f"
+                student.GivenName student.Id student.MeanScore student.MinScore student.MaxScore)
+
     let summerize filePath =
         let rows = File.ReadAllLines filePath
         let studentCount = (rows |> Array.length) - 1
@@ -11,5 +20,7 @@ module Summary =
         rows
         |> Array.skip 1
         |> Array.map Student.fromString
-        |> Array.sortByDescending (fun student -> student.MeanScore)
-        |> Array.iter Student.printSummary
+        |> Array.groupBy (fun s -> s.Surname)
+        |> Array.sortBy fst
+        |> Array.iter (fun (surname, students) ->
+            printGroupSummary surname students)
