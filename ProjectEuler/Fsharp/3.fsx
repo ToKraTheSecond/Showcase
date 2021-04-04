400c0dd302d9d8c1e83fb642f703c6b29c914edd
ToKra <kratas.tom@gmail.com>
2021-04-04 20:47:36 +0200

    call factorize function from module

--- ProjectEuler/Fsharp/3.fsx
+++ ProjectEuler/Fsharp/3.fsx
@@ -1,12 +1,7 @@
 // inspiration on how to work with numbers bigger than 32-bit integer from:
 // https://jeremybytes.blogspot.com/2016/07/getting-prime-factors-in-f-with-good.html
+#load "factorization.fs"
 
-let rec factorize n possibleFactor factorSet =
-    if possibleFactor = n then
-        possibleFactor::factorSet
-    elif n % possibleFactor = 0L then
-        factorize (n / possibleFactor) possibleFactor (possibleFactor::factorSet)
-    else
-        factorize n (possibleFactor + 1L) factorSet
+open ProjectEuler.Factorization
 
 factorize 600851475143L 2L [] |> Seq.max
