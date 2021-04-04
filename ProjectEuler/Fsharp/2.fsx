bd2b6c8bc495445f6476f7c96495bbb9c13e738d
ToKra <kratas.tom@gmail.com>
2021-04-04 17:09:35 +0200

    add second problem solution

---
+++ ProjectEuler/Fsharp/2.fsx
@@ -0,0 +1,17 @@
+// My solution
+Seq.unfold (fun (a, b) -> Some(a + b, (b, a + b))) (0, 1)
+|> Seq.takeWhile (fun x -> x <= 3999999 )
+|> Seq.filter (fun x -> x % 2 = 0)
+|> Seq.sum
+
+// Other cool solutions
+// https://stackoverflow.com/a/45776903
+let rec fib a b =
+    let current = a + b
+    match current with
+    | _ when current >= 3999999 -> []
+    | _ -> current :: fib b current 
+
+fib 0 1
+|> Seq.filter (fun x -> x % 2 = 0)
+|> Seq.sum
