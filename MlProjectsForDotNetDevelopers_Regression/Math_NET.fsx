ae92c74d88e48b412fe6fd63a7d93299ac7594b9
ToKra <kratas.tom@gmail.com>
2021-05-05 23:33:14 +0200

    adding continuous features to the model

--- MlProjectsForDotNetDevelopers_Regression/Math_NET.fsx
+++ MlProjectsForDotNetDevelopers_Regression/Math_NET.fsx
@@ -30,6 +30,7 @@ type Mat = Matrix<float>
 
 type Obs = Data.Row
 type Model = Obs -> float
+type Featurizer = Obs -> float list
 
 let update alpha (theta0, theta1) (obs:Obs) =
     let y = float obs.Cnt
@@ -53,3 +54,79 @@ predict theta (X.Row(0))
 cost theta Y X
 
 let estimate (Y:Vec) (X:Mat) = (X.Transpose() * X).Inverse() * X.Transpose() * Y
+
+let seed = 314159
+let rng = System.Random(seed)
+
+// Fisher-Yates shuffle
+let shuffle (arr:'a []) =
+    let arr = Array.copy arr
+    let l = arr.Length
+    for i in (l-1) .. -1 .. 1 do
+        let temp = arr.[i]
+        let j = rng.Next(0,i+1)
+        arr.[i] <- arr.[j]
+        arr.[j] <- temp
+    arr
+
+let training,validation =
+    let shuffled =
+        data
+        |> Seq.toArray
+        |> shuffle
+    let size = 0.7 * float (Array.length shuffled) |> int
+    shuffled.[..size],
+    shuffled.[size+1..]
+
+let exampleFeaturizer (obs:Obs) = [ 1.0; float obs.Instant; ]
+let predictor (f:Featurizer) (theta:Vec) = f >> vector >> (*) theta
+
+let evaluate (model:Model) (data:Obs seq) =
+    data
+    |> Seq.averageBy (fun obs ->
+        abs (model obs - float obs.Cnt))
+
+let model (f:Featurizer) (data:Obs seq) =
+    let Yt, Xt =
+        data
+        |> Seq.toList
+        |> List.map (fun obs -> float obs.Cnt, f obs)
+        |> List.unzip
+    let theta = estimate (vector Yt) (matrix Xt)
+    let predict = predictor f theta
+    theta,predict
+
+let featurizer0 (obs:Obs) = [ 1.; float obs.Instant; ]
+let (theta0,model0) = model featurizer0 training
+
+evaluate model0 training |> printfn "Training: %.0f"
+evaluate model0 validation |> printfn "Validation: %.0f"
+
+let featurizer1 (obs:Obs) =
+    [
+        1.
+        obs.Instant |> float
+        obs.Atemp |> float
+        obs.Hum |> float
+        obs.Temp |> float
+        obs.Windspeed |> float
+    ]
+
+let (theta1,model1) = model featurizer1 training
+
+evaluate model1 training |> printfn "Training: %.0f"
+evaluate model1 validation |> printfn "Validation: %.0f"
+
+let xAxisRange = [ 0 .. [ for obs in data -> float obs.Cnt ].Length]
+
+[
+    Scatter( x = xAxisRange, y = [ for obs in data -> float obs.Cnt ] );
+    Scatter( x = xAxisRange, y = [ for obs in data -> model0 obs ] );
+    Scatter( x = xAxisRange, y = [ for obs in data -> model1 obs ]);
+]
+|> Chart.Plot
+|> Chart.Show
+
+[ for obs in data -> float obs.Cnt, model1 obs ]
+|> Chart.Scatter
+|> Chart.Show
