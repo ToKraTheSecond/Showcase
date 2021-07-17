b3e6f443fe14aaee03b202b980b50d72653f6835
ToKra <kratas.tom@gmail.com>
2021-07-17 18:49:36 +0200

    correct typo

--- MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/KMeans.fs
+++ MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/KMeans.fs
@@ -1,5 +1,7 @@
 namespace Unsupervised
+
 module KMeans =
+
     let pickFrom size k =
         let rng = System.Random ()
         let rec pick (set:int Set) =
@@ -10,52 +12,55 @@ module KMeans =
         pick Set.empty |> Set.toArray
 
     let initialize observations k =
+
         let size = Array.length observations
 
-        let centroids =
+        let centroids = 
             pickFrom size k
-            |> Array.mapi (fun i index ->
+            |> Array.mapi (fun i index -> 
                 i+1, observations.[index])
-        
-        let assignments =
-            observations
+
+        let assignments = 
+            observations 
             |> Array.map (fun x -> 0, x)
 
-        (assignments, centroids)
+        (assignments,centroids)
 
     let clusterize distance centroidOf observations k =
+
         let rec search (assignments,centroids) =
             // Centroid ID of the closest centroid
-            let classifier observation =
-                centroids
-                |> Array.minBy (fun (_,centroid) ->
+            let classifier observation =  
+                centroids 
+                |> Array.minBy (fun (_,centroid) -> 
                     distance observation centroid)
                 |> fst
             // Assign each observation to closest centroid
-            let assignments' =
-                assignments
+            let assignments' = 
+                assignments 
                 |> Array.map (fun (_,observation) ->
-                    let closestCentroidId = classifier observations
+                    let closestCentroidId = classifier observation
                     (closestCentroidId,observation))
             // Check if any observation changed cluster
-            let changed =
-                (assignments,assignments')
-                ||> Seq.zip
-                |> Seq.exists (fun ((oldClusterID,_),(newClusterID,_)) ->
+            let changed = 
+                (assignments,assignments') 
+                ||> Seq.zip 
+                |> Seq.exists (fun ((oldClusterID,_),(newClusterID,_)) -> 
                     not (oldClusterID = newClusterID))
-            
-            if changed
+
+            if changed 
             then
-                let centroids' =
-                    assignments'
+                let centroids' = 
+                    assignments' 
                     |> Seq.groupBy fst
-                    |> Seq.map (fun (clusterID, group) ->
+                    |> Seq.map (fun (clusterID, group) -> 
                         clusterID, group |> Seq.map snd |> centroidOf)
                     |> Seq.toArray
+                // note: centroids could vanish,
+                // i.e. K decreases
                 search (assignments',centroids')
             else centroids,classifier
 
-            // start searching from initial values
-
+        // start searching from initial values
         let initialValues = initialize observations k
         search initialValues
