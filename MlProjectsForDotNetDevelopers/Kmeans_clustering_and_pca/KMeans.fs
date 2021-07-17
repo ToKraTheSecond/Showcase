a0335b7bd1db1684a156b0943f874e31a9a01aa1
ToKra <kratas.tom@gmail.com>
2021-07-17 17:37:04 +0200

    recursive clusters update

--- MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/KMeans.fs
+++ MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/KMeans.fs
@@ -22,3 +22,40 @@ module KMeans =
             |> Array.map (fun x -> 0, x)
 
         (assignments, centroids)
+
+    let clusterize distance centroidOf observations k =
+        let rec search (assignments,centroids) =
+            // Centroid ID of the closest centroid
+            let classifier observation =
+                centroids
+                |> Array.minBy (fun (_,centroid) ->
+                    distance observation centroid)
+                |> fst
+            // Assign each observation to closest centroid
+            let assignments' =
+                assignments
+                |> Array.map (fun (_,observation) ->
+                    let closestCentroidId = classifier observations
+                    (closestCentroidId,observation))
+            // Check if any observation changed cluster
+            let changed =
+                (assignments,assignments')
+                ||> Seq.zip
+                |> Seq.exists (fun ((oldClusterID,_),(newClusterID,_)) ->
+                    not (oldClusterID = newClusterID))
+            
+            if changed
+            then
+                let centroids' =
+                    assignments'
+                    |> Seq.groupBy fst
+                    |> Seq.map (fun (clusterID, group) ->
+                        clusterID, group |> Seq.map snd |> centroidOf)
+                    |> Seq.toArray
+                search (assignments',centroids')
+            else centroids,classifier
+
+            // start searching from initial values
+
+        let initialValues = initialize observations k
+        search initialValues
