695ff64d698f75256353cc155c9f2db35cf70a9d
ToKra <kratas.tom@gmail.com>
2021-07-17 18:38:11 +0200

    clustering the dataset

--- MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/Script.fsx
+++ MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/Script.fsx
@@ -48,11 +48,22 @@ type Observation = float []
 let features = headers.Length
 
 let distance (obs1:Observation) (obs2:Observation) =
-    (obs1, obs2)
+    (obs1, obs2) 
     ||> Seq.map2 (fun u1 u2 -> pown (u1 - u2) 2)
     |> Seq.sum
+    |> sqrt
 
 let centroidOf (cluster:Observation seq) =
     Array.init features (fun f ->
-        cluster
+        cluster 
         |> Seq.averageBy (fun user -> user.[f]))
+
+let observations1 = 
+    observations 
+    |> Array.map (Array.map float) 
+    |> Array.filter (fun x -> Array.sum x > 0.)
+
+let (clusters1, classifier1) = 
+    let clustering = clusterize distance centroidOf
+    let k = 5
+    clustering observations1 k
