cb66a92a9062c8fea17fd70c2f225fb96dc33ab7
ToKra <kratas.tom@gmail.com>
2021-07-19 10:40:23 +0200

    use AIC for k estimation

--- MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/Script.fsx
+++ MlProjectsForDotNetDevelopers/Kmeans_clustering_and_pca/Script.fsx
@@ -100,5 +100,34 @@ observations2
     printfn "Cluster %i: %i elements" clusterID count)
 
 let ruleOfThumb (n:int) = sqrt (float n / 2.)
-let k_uleOfThumb = ruleOfThumb observations2.Length
+let k_ruleOfThumb = ruleOfThumb observations2.Length
 
+let squareError (obs1:Observation) (obs2:Observation) =
+    (obs1,obs2)
+    ||> Seq.zip
+    |> Seq.sumBy (fun (x1,x2) -> pown (x1-x2) 2)
+
+let RSS (dataset:Observation[]) centroids =
+    dataset
+    |> Seq.sumBy (fun obs -> 
+        centroids
+        |> Seq.map (squareError obs)
+        |> Seq.min)
+
+let AIC (dataset:Observation[]) centroids =
+    let k = centroids |> Seq.length
+    let m = dataset.[0] |> Seq.length
+    RSS dataset centroids + float (2 * m * k)
+
+[1..25]
+|> Seq.map (fun k ->
+    let value =
+        [ for _ in 1 .. 10 -> 
+            let (clusters, classifier) =
+                let clustering = clusterize distance centroidOf
+                clustering observations2 k
+            AIC observations2 (clusters |> Seq.map snd)]
+        |> List.average
+    k, value)
+|> Chart.Line
+|> Chart.Show
